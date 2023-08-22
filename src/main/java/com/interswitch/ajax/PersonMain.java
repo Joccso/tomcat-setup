fc1ec299ede2e092c283a27a8026091752e61f05
lerderl <josepholukunle1107@gmail.com>
2023-08-22 15:03:57 +0100

    feat: convert to xml

---
+++ src/main/java/com/interswitch/ajax/PersonMain.java
@@ -0,0 +1,22 @@
+package com.interswitch.ajax;
+
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
+
+import java.io.StringWriter;
+
+public class PersonMain {
+  public static void main(String[] args) throws JAXBException {
+    Person person = new Person();
+    person.setName("James Femi-Oyetoro");
+    person.setAddress("Abeokuta, Nigeria");
+
+    JAXBContext context = JAXBContext.newInstance(Person.class);
+    Marshaller marshaller = context.createMarshaller();
+
+    StringWriter writer = new StringWriter();
+    marshaller.marshal(person, writer);
+    System.out.println(writer.toString());
+  }
+}
