b777ff193f2f7a4b1d9895c19d82b45c88c76a09
lerderl <josepholukunle1107@gmail.com>
2023-08-23 16:32:33 +0100

    feat: convert employee to json

--- src/main/java/com/interswitch/ajax/classwork/EmployeeMain.java
+++ src/main/java/com/interswitch/ajax/classwork/EmployeeMain.java
@@ -3,8 +3,12 @@ package com.interswitch.ajax.classwork;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.Unmarshaller;
 import jakarta.xml.bind.JAXBException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeeMain {
   public static void main(String[] args) throws JAXBException {
@@ -13,5 +17,21 @@ public class EmployeeMain {
     File file = new File("employee.xml");
     Employee employee = (Employee) unmarshaller.unmarshal(file);
     System.out.println(employee);
+    /* Convert employee to Json object */
+    JSONObject jsonObject = new JSONObject(employee);
+    System.out.println(jsonObject);
+    Employee employee1 = (Employee) unmarshaller.unmarshal(file);
+    Employee employee2 = (Employee) unmarshaller.unmarshal(file);
+    /* Convert employee list to Json array */
+//    List<String> employeeList = new ArrayList<>();
+//    employeeList.add(employee.toString());
+//    employeeList.add(employee1.toString());
+//    employeeList.add(employee2.toString());
+//    JSONArray jsonArray = new JSONArray(employeeList);
+    JSONArray jsonArray = new JSONArray(List.of(employee, employee1, employee2));
+    System.out.println(jsonArray);
+    for (int i = 0; i < jsonArray.length(); i++) {
+      System.out.println(jsonArray.get(i));
+    }
   }
 }
