a0609520850256a1faa94192b23db97b80c2045d
lerderl <josepholukunle1107@gmail.com>
2023-08-23 14:53:27 +0100

    feat: mapping with json

--- src/main/java/com/interswitch/json/JSONTest.java
+++ src/main/java/com/interswitch/json/JSONTest.java
@@ -2,6 +2,9 @@ package com.interswitch.json;
 
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class JSONTest {
   public static void main (String[] args) {
     JSONObject jo = new JSONObject();
@@ -9,5 +12,15 @@ public class JSONTest {
     jo.put("age", 22);
     jo.put("city", "Chicago");
     System.out.println(jo);
+    /* Mapping with Json */
+    System.out.println("Mapping with Json");
+    Map<String, String> map = new HashMap<>();
+    map.put("name", "Jon Doe");
+    map.put("age", "22");
+    map.put("city", "Chicago");
+    System.out.println(map);
+    JSONObject jo1 = new JSONObject(map);
+    System.out.println(jo1);
+
   }
 }
